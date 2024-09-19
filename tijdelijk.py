56802201e1e5ec53ce947d71362ab8c4755d3c64
Nils De Velder <nilsdevelder@gmail.com>
2024-09-19 17:05:38 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,16 @@
+prijzen={
+    "aardbei" : "3",
+    "vanille" : "4",
+    "chocolade" : "5"
+}
+A= int(prijzen["aardbei"])
+aanbieding= A*0.8
+reclame_tekst= (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding}")
+reclame_tekst2= reclame_tekst[:61]
+reclame_tekst3= (reclame_tekst2.upper())
+reclame_tekst4= (reclame_tekst3.split())
+for i in (reclame_tekst4):
+    if len(i) >= 5:
+     print(i.upper()) and print(i.lower())
+    else:
+       print(i.lower()) and print(i.upper())
